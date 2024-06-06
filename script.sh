1464c53ab5d4e4b4f85c164c56082875d5697971
Davide Tazzioli <davide.tazzioli@unibo.it>
2024-06-06 17:29:54 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,6 +3,6 @@ mkdir -p emqx && tar -zxvf emqx-5.7.0-ubuntu22.04-amd64.tar.gz -C emqx
 echo '
 api_key = {
   bootstrap_file = "etc/default_api_key.conf"
-}'> ~/emqx/etc/emqx.conf
-echo 'my-app:AAA4A275-BEEC-4AF8-B70B-DAAC0341F8EB'> ~/emqx/etc/default_api_key.conf
+}'>> ~/emqx/etc/emqx.conf
+echo 'my-app:AAA4A275-BEEC-4AF8-B70B-DAAC0341F8EB'>> ~/emqx/etc/default_api_key.conf
 ./emqx/bin/emqx start
